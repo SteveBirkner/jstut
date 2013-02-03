2b2758bc60c650a3c39afa6d17d3dd348e3c646d
Steven Birkner <stevebirk36@gmail.com>
2013-02-02 19:21:19 -0500

    functions

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-var grade = "Prem";
-
-switch (grade){
-	case "reg" : 
-		alert ("cool");
-		break;
-	case "Prem" : 
-		alert("Awesome");
-		break;
-	default: 
-		alert("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-	
+function myFunc () {
+	alert("zomg function");
 }
+
+
+myFunc();
+
+
